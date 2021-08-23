5a874d61ad811df9a39effa711890dd016b0e754
luipalencia <luipalencia@gmail.com>
2021-08-23 17:30:55 -0400

    development environment configuration (create-app)

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from 'react'
+import NotFound from './views/NotFound'
+import Home from './views/Home'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="*">
+             <NotFound />
+            </Route> 
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/views/Home.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+const Home = () => {
+    return (  
+        <div className="home">
+<h1>Test</h1>
+      </div> 
+    );
+}
+ 
+export default Home;
